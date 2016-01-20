2a85e4997d8c11d352b0ce4847b9c88e06105e0b
Gustavo Ajzenman <gustavoajz@gmail.com>
2016-01-20 16:02:26 -0300

    Fix drone yml

--- .drone/create_container.sh
+++ .drone/create_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LAST_COMMIT=git log -n 1 --pretty=format:%h  -- package.json
+LAST_COMMIT=$(git log -n 1 --pretty=format:%h  -- package.json)
 
 if [ -z "$LAST_COMMIT" ]; then
   LAST_COMMIT="default"
